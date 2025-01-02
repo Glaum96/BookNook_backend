e23900858f36430c8374c05c729ae995b199d34d
Kristin Aune <kristin.aune@sparebank1.no>
2025-01-02 12:30:09 +0100

    Fikset småring i backednf or admin-side

--- src/main/kotlin/com/users/UserController.kt
+++ src/main/kotlin/com/users/UserController.kt
@@ -1,10 +1,10 @@
 package com.users
 
 import com.users.model.User
-import com.users.model.getUsers
 import com.users.model.postNewUser
 import com.google.gson.Gson
 import com.users.model.getUserFromDB
+import com.users.model.getUsersFromDb
 import com.users.model.putUser
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -22,10 +21,10 @@ import org.springframework.web.bind.annotation.RestController
 class GetUsersController {
     @GetMapping
     fun getAllUsers(): List<User> {
-        return  getUsers()
+        return  getUsersFromDb()
     }
-
 }
+
 @RestController
 @RequestMapping("/api/postUsers")
 @CrossOrigin(origins = ["http://localhost:5173"], allowedHeaders = ["*"], allowCredentials = "true")
