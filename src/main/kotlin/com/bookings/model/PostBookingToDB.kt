8c620dd5d9cd10c273852bfa4038e378aceb3f9b
Kristin Aune <kristin.aune@sparebank1.no>
2025-03-20 12:58:13 +0100

    Lagt inn sjekk på at bookingen ikke overlapper eksisterende bookinger

--- src/main/kotlin/com/bookings/model/PostBookingToDB.kt
+++ src/main/kotlin/com/bookings/model/PostBookingToDB.kt
@@ -4,29 +4,47 @@ import com.main.model.createMongoClient
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.reactive.awaitFirstOrNull
 import org.bson.Document
+import com.mongodb.client.model.Filters
 
-fun postBookingToDB(newBooking: Booking) = runBlocking {
+fun isBookingAvailable(newBooking: Booking): Boolean = runBlocking {
+    val uri = "mongodb+srv://takterrassen:Seilduksgata6B@takterrassen.jz3qs.mongodb.net/?retryWrites=true&w=majority&appName=Takterrassen"
+    val mongoClient = createMongoClient(uri)
+    val database = mongoClient.getDatabase("Bookings")
+    val collection = database.getCollection("Bookings")
+
+    val overlappingBooking = collection.find(
+        Filters.and(
+            Filters.lt("from", newBooking.endTime),  // Startet før den nye bookingen slutter
+            Filters.gt("to", newBooking.startTime)   // Sluttet etter den nye bookingen startet
+        )
+    ).awaitFirstOrNull()
+
+    mongoClient.close()
+    return@runBlocking overlappingBooking == null
+}
+
+// Legger til en booking hvis tidsrommet er ledig
+fun postBookingToDB(newBooking: Booking): Boolean = runBlocking {
+    if (!isBookingAvailable(newBooking)) {
+        return@runBlocking false // Returner false hvis tidsrommet er opptatt
+    }
 
     val uri = "mongodb+srv://takterrassen:Seilduksgata6B@takterrassen.jz3qs.mongodb.net/?retryWrites=true&w=majority&appName=Takterrassen"
-
     val mongoClient = createMongoClient(uri)
 
     val database = mongoClient.getDatabase("Bookings")
     val collection = database.getCollection("Bookings")
-    val booking = mutableListOf<Booking>()
 
-    runBlocking {
-        val userDocument = Document()
-            .append("from", newBooking.startTime)
-            .append("to", newBooking.endTime)
-            .append("userId", newBooking.userId)
-            .append("responsibleName", newBooking.responsibleName)
-            .append("responsibleNumber", newBooking.responsibleNumber)
+    val bookingDocument = Document()
+        .append("from", newBooking.startTime)
+        .append("to", newBooking.endTime)
+        .append("userId", newBooking.userId)
+        .append("responsibleName", newBooking.responsibleName)
+        .append("responsibleNumber", newBooking.responsibleNumber)
 
-        collection.insertOne(userDocument).awaitFirstOrNull()
-    }
-
+    collection.insertOne(bookingDocument).awaitFirstOrNull()
 
     mongoClient.close()
-    return@runBlocking booking
+    return@runBlocking true
 }
+
