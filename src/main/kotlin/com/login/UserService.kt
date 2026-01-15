62767d36208dc6b72145f1720c6220b48295ceda
Haakon Gunleiksrud <haakon.gunleiksrud@sparebank1.no>
2026-01-15 15:30:57 +0100

    --wip-- [skip ci]

--- src/main/kotlin/com/login/UserService.kt
+++ src/main/kotlin/com/login/UserService.kt
@@ -1,6 +1,7 @@
 package com.login
 
 import com.login.model.LoginCredentials
+import com.login.model.RegisterUser
 import com.login.model.getUserByUsername
 import com.users.model.getUserFromDB
 import org.springframework.beans.factory.annotation.Autowired
@@ -28,5 +29,24 @@ class UserService {
 
     fun userIsAdmin(userName: String): Boolean {
         return getUserFromDB(userName)?.isAdmin ?: false
+    }
+
+    fun validateNewUser(user: RegisterUser): Boolean {
+        if (user.name.isBlank() || user.email.isBlank() || user.password.isBlank()) {
+            return false
+        }
+
+        val emailAddressAlreadyExists = getUserByUsername(user.email) != null
+        if (emailAddressAlreadyExists) {
+            return false
+        }
+
+        val emailHasValidFormat = Regex("^[A-Za-z0-9](.*)([@])(.+)(\\.)(.+)").matches(user.email)
+        if (!emailHasValidFormat) {
+            return false
+        }
+
+        // Additional validation logic can be added here (e.g., email format, password strength)
+        return true
     }
 }
