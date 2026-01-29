a6526b038ba26970e3146e14302628a18e795c9b
Haakon Gunleiksrud <haakon.gunleiksrud@sparebank1.no>
2026-01-29 15:22:36 +0100

    Add timestamp to ping api end point

--- src/main/kotlin/com/main/MainController.kt
+++ src/main/kotlin/com/main/MainController.kt
@@ -3,11 +3,14 @@ package com.main
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @RestController
 @RequestMapping("/")
 class MainController {
     @get:GetMapping
     val main: String
-        get() = "Hei på deg fra backenden!"
+        get() = "Hei på deg fra backenden! Klokka er: " +
+            LocalDateTime.now().format(DateTimeFormatter.ofPattern("d. MMMM yyyy, HH:mm:ss"))
 }
